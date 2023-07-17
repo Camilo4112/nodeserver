b9c96bfb4182d0ed1580d994b5e6ce42121c964b
juan camilo solorzano gonzalez <solorzano2002gonzalez@gmail.com>
2023-07-16 22:20:24 -0500

    Autenticación en Express

--- server.js
+++ server.js
@@ -1,33 +1,58 @@
 const express = require('express');
 const app = express();
-
-// Middleware para validar métodos HTTP
-const validateMethod = (req, res, next) => {
-  if (!['GET', 'POST', 'PUT', 'DELETE'].includes(req.method)) {
-    return res.status(400).json({ error: 'Método HTTP no válido' });
-  }
-  next();
-};
-
-// Middleware para manejar errores en el servidor
-const errorHandler = (err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ error: 'Ocurrió un error en el servidor' });
-};
+const jwt = require('jsonwebtoken');
 
 app.use(express.json());
 
-// Middleware a nivel de aplicación para validar métodos HTTP
-app.use(validateMethod);
+const users = [
+  {
+    id: 1,
+    username: 'usuario1',
+    password: 'contrasena1'
+  },
+  {
+    id: 2,
+    username: 'usuario2',
+    password: 'contrasena2'
+  }
+];
 
-const listViewRouter = require('./list-view-router');
-const listEditRouter = require('./list-edit-router');
+const jwtSecret = process.env.JWT_SECRET || 'mysecretpassword';
 
-app.use('/list-view', listViewRouter);
-app.use('/list-edit', listEditRouter);
+const generateToken = (user) => {
+  return jwt.sign({ id: user.id, username: user.username }, jwtSecret, { expiresIn: '1h' });
+};
 
-// Middleware para manejo de errores en el servidor
-app.use(errorHandler);
+const authenticateToken = (req, res, next) => {
+  const token = req.header('Authorization');
+  if (!token) {
+    return res.status(401).json({ error: 'Token no proporcionado' });
+  }
+
+  jwt.verify(token, jwtSecret, (err, user) => {
+    if (err) {
+      return res.status(403).json({ error: 'Token inválido' });
+    }
+    req.user = user;
+    next();
+  });
+};
+
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+  const user = users.find(user => user.username === username && user.password === password);
+
+  if (!user) {
+    return res.status(401).json({ error: 'Credenciales inválidas' });
+  }
+
+  const token = generateToken(user);
+  res.json({ token });
+});
+
+app.get('/protected', authenticateToken, (req, res) => {
+  res.json({ message: 'Ruta protegida alcanzada', user: req.user });
+});
 
 const port = 3000;
 app.listen(port, () => {
